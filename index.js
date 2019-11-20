3e0d82fe886cde4aef6ef674c344fc51d50359e8
Anupama Gadde <anupamagadde@yahoo.com>
2019-11-20 19:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, { [key]: value} );
 }
 function deleteFromObjectByKey(object, key) {
-var newObject =  Object.assign({}, object, { [key]: value});
+var newObject =  Object.assign({}, object);
 delete newObject[key];
   return newObject;
 }
